82cf82cceb5c2f0df651a41c0b0ea8717ee68f7e
ren-yamanashi <pinokionigg@icloud.com>
2025-04-15 23:22:34 +0900

    docs: update

--- examples/src/require-if-block.ts
+++ examples/src/require-if-block.ts
@@ -1,8 +1,25 @@
 import { Rule } from "eslint";
 
 export const requireIfBlock: Rule.RuleModule = {
-  meta: {},
-  create() {
-    return {};
+  meta: {
+    type: "problem",
+    docs: {
+      description: "if文を Block Statement で記述することを強制する",
+    },
+    messages: {
+      requireIfBlock: "if文にブロックを使用してください",
+    },
+  },
+  create(context) {
+    return {
+      IfStatement(node) {
+        if (node.consequent.type !== "BlockStatement") {
+          context.report({
+            node,
+            messageId: "requireIfBlock",
+          });
+        }
+      },
+    };
   },
 };
